890ca20004c803f8ab3d40c4f0c579ecdf14e82a
IdrisFallout <waithakasam2017@gmail.com>
2023-08-03 18:17:56 +0300

    API Authorization

---
+++ DatabaseModule/database.py
@@ -0,0 +1,92 @@
+import mysql.connector
+import bcrypt
+
+# MySQL configuration
+db_config = {
+    'host': 'localhost',
+    'user': 'root',
+    'password': '',
+    'database': 'crime_reporting_db',
+}
+
+
+def connect_to_database():
+    try:
+        connection = mysql.connector.connect(**db_config)
+        cursor = connection.cursor()
+        return connection, cursor
+    except mysql.connector.Error as err:
+        pass
+        # print("Error connecting to MySQL: ", err)
+        return None, None
+
+
+def fetch_users():
+    connection, cursor = connect_to_database()
+    if connection and cursor:
+        try:
+            query = "SELECT * FROM login_table"
+            cursor.execute(query)
+            columns = [column[0] for column in cursor.description]
+            users = []
+            for row in cursor.fetchall():
+                user_dict = {}
+                for i, col in enumerate(columns):
+                    user_dict[col] = row[i]
+                users.append(user_dict)
+            return users
+        except mysql.connector.Error as err:
+            # print("Error executing query: ", err)
+            pass
+        finally:
+            cursor.close()
+            connection.close()
+    return None
+
+
+def register_user(phone, full_name, pin):
+    connection, cursor = connect_to_database()
+    if connection and cursor:
+        try:
+            query = "INSERT INTO login_table (phone, full_name, pin) VALUES (%s, %s, %s)"
+            values = (phone, full_name, password_to_hash(pin))
+            cursor.execute(query, values)
+            connection.commit()
+            return True  # Return True to indicate successful insertion
+        except mysql.connector.Error as err:
+            # print("Error executing query: ", err)
+            connection.rollback()  # Rollback the changes in case of an error
+        finally:
+            cursor.close()
+            connection.close()
+    return False  # Return False to indicate failure in insertion
+
+
+def login_user(phone):
+    connection, cursor = connect_to_database()
+    if connection and cursor:
+        try:
+            query = "SELECT pin FROM login_table WHERE phone = %s"
+            values = (phone,)
+            cursor.execute(query, values)
+            result = cursor.fetchone()
+            if result:
+                return result[0]  # Return the hashed password
+            else:
+                return None  # Return None if the phone number is not found
+        except mysql.connector.Error as err:
+            pass
+            # print("Error executing query: ", err)
+        finally:
+            cursor.close()
+            connection.close()
+    return None  # Return None if there's an issue with the database connection
+
+
+def password_to_hash(password):
+    hashed = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+    return hashed.decode('utf-8')
+
+
+def hash_to_password(password, hashed):
+    return bcrypt.checkpw(password.encode('utf-8'), hashed.encode('utf-8'))
--- auth/auth.py
+++ auth/auth.py
@@ -1,10 +1,27 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
+from DatabaseModule.database import fetch_users, register_user, login_user, hash_to_password
 
 auth = Blueprint('auth', __name__)
 
 
 @auth.route('/login', methods=['POST'])
 def login():
+    phone = request.json['phone']
+    pin = request.json['pin']
+
+    if login_user(phone) is None:
+        response = {
+            'responseType': 'error',
+            'message': 'Invalid phone or pin'
+        }
+        return jsonify(response), 200
+
+    if not hash_to_password(pin, login_user(phone)):
+        response = {
+            'responseType': 'error',
+            'message': 'Invalid phone or pin'
+        }
+        return jsonify(response), 200
     response = {
         'responseType': 'success',
         'message': 'Login successful'
@@ -12,10 +29,41 @@ def login():
     return jsonify(response), 200
 
 
-@auth.route('/signup', methods=['POST'])
-def signup():
+@auth.route('/get_users', methods=['GET'])
+def get_user():
     response = {
         'responseType': 'success',
-        'message': 'Signup successful'
+        'message': fetch_users()
     }
+    return jsonify(response), 200
+
+
+@auth.route('/signup', methods=['POST'])
+def register():
+    phone = request.json['phone']
+    full_name = request.json['full_name']
+    pin = request.json['pin']
+    if len(str(pin)) != 4:
+        response = {
+            'responseType': 'error',
+            'message': 'Pin must be 4 digits'
+        }
+        return jsonify(response), 200
+    if not str(pin).isdigit():
+        response = {
+            'responseType': 'error',
+            'message': 'Pin must be a number'
+        }
+        return jsonify(response), 200
+    if register_user(phone, full_name, pin):
+        response = {
+            'responseType': 'success',
+            'message': 'Signup successful'
+        }
+        return jsonify(response), 200
+    else:
+        response = {
+            'responseType': 'error',
+            'message': 'Signup failed'
+        }
     return jsonify(response), 200
